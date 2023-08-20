8163adcc7de57e64d49d5af99699723f70c15d97
DHEEPA K <69415255+dheepa29@users.noreply.github.com>
2023-08-20 16:17:55 +0530

    Create Hierarchy.java

---
+++ Finzly_Assesments/Assesments/src/Hierarchy.java
@@ -0,0 +1,14 @@
+public class Main {
+		
+	void show(String s){
+		System.out.println("String method");
+	}
+	void show(Object o){
+		System.out.println("Object method");
+	}
+		
+	public static void main(String[] args) {
+		Main main = new Main();
+		main.show((Object)null);
+	}
+}
